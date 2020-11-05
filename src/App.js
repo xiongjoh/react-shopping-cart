f2acf35dcd6bfca9a1b9d579b8ad764bc0f2da8b
unknown <xiongjoh@gmail.com>
2020-11-05 15:42:52 -0600

    add ability to add multiple items of same type and remove them

--- src/App.js
+++ src/App.js
@@ -17,11 +17,19 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
-		setCart([...cart, item])
+		// console.log(item.id)
+		// console.log(cart.some(existingItem => existingItem.id === item.id))
+		setCart([...cart, {...item, id:item.id+Date.now()}])
 	};
 
+	const removeItem = itemId => {
+		const cartIndex = cart.findIndex(item => item.id === itemId)
+		const newCart = cart.filter((item, idx) => idx != cartIndex)
+		setCart([...newCart])
+	}
+
 	return (
-		<ProductContext.Provider value={{ products, addItem}}>
+		<ProductContext.Provider value={{ products, addItem, removeItem}}>
 			<CartContext.Provider value={{cart}}>
 				<div className="App">
 					<Navigation />
